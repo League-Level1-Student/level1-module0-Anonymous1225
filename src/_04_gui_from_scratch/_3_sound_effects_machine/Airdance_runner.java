91d83b615e3c222f8145b1c85435f09c78d660d2
powerrspyy <powerrspyy@gmail.com>
2024-09-28 17:57:10 -0700

    Funny sounds

---
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/Airdance_runner.java
@@ -0,0 +1,10 @@
+package _04_gui_from_scratch._3_sound_effects_machine;
+
+import _04_gui_from_scratch._2_jack_in_the_box.Jack;
+import java.util.Random;
+public class Airdance_runner {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Airdance().run();
+	}
+}
